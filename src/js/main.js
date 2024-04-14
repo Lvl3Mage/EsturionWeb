2c6c00a312c79912b70c4a6a4c3a446339019de0
Lvl3Mage <lvlthreemage@gmail.com>
2024-04-14 19:48:43 +0200

    Minor fixes

--- src/js/main.js
+++ src/js/main.js
@@ -100,6 +100,8 @@ $(document).on("mousemove", function(e){
 
 window.InitializeDropdowns = InitializeDropdowns;
 window.InitializeSliders = InitializeSliders;
+window.OpenModal = OpenModal;
+window.CloseModal = CloseModal;
 $(document).ready(function(){
 	InitializeDropdowns();
 	InitializeSliders();
@@ -219,22 +221,27 @@ function InitializeSliders(){
 		slidesToScroll: 1,
 		infinite: true,
 		focusOnSelect: true,
-		dots: false,
-		arrows:true,
+		dots: true,
+		arrows:false,
 		prevArrow: `<button type="button" class="slick-prev text-4xl top-0 bottom-0 m-auto text-white aria-disabled:text-gray-300 absolute left-3 z-10">
-			<div class="border-2 border-white rounded-full flex justify-center items-center py-5 px-4">
-				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="37" viewBox="0 0 20 37" fill="none">
+			<div class="border-2 border-white rounded-full flex justify-center items-center py-5 px-4 md:p-2">
+				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="37" viewBox="0 0 20 37" fill="none" class="w-5 md:w-3">
 					<path d="M18.0625 36L1 18.9375L18.0625 1.875" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 				</svg>
 			</div>
 		</button>`,
 		nextArrow: `<button type="button" class="slick-prev text-4xl top-0 bottom-0 m-auto text-white aria-disabled:text-gray-300 absolute right-3 z-10">
-			<div class="border-2 border-white rounded-full flex justify-center items-center py-5 px-4 rotate-180">
-				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="37" viewBox="0 0 20 37" fill="none">
+			<div class="border-2 border-white rounded-full flex justify-center items-center py-5 px-4 md:p-2 rotate-180">
+				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="37" viewBox="0 0 20 37" fill="none" class="w-5 md:w-3">
 					<path d="M18.0625 36L1 18.9375L18.0625 1.875" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 				</svg>
 			</div>
 		</button>`,
+		dotsClass: 'absolute top-[70px] right-0 left-0 h-8 *:p-3 flex justify-center gap-2 *:[&.slick-active]:*:outline-white *:hover:*:brightness-90 *:rounded-full overflow-hidden',
+		customPaging : function(slider, i) {
+			return `<div class="bg-white cursor-pointer w-2 h-2 transition-all duration-300 ease-out-wobble rounded-full outline-offset-4 outline outline-transparent outline-2">
+			</div>`;
+		},
 	});
 	
 }
