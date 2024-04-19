411e6f240350dc3904df033a5b579e0bd110a4ed
Lvl3Mage <lvlthreemage@gmail.com>
2024-04-19 17:57:41 +0200

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -261,6 +261,6 @@ function EnableSliderFiltering(sliderSelector, selector){
 	$(sliderSelector).slick('slickUnfilter');
 	$(sliderSelector).slick('slickFilter', selector);
 }
-function DisableSliderFiltering(){
+function DisableSliderFiltering(sliderSelector){
 	$(sliderSelector).slick('slickUnfilter');
 }
