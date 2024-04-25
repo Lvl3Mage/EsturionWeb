bded8ab4b52fcd5a3ed9986f2955ae4b4daf7b71
Lvl3Mage <lvlthreemage@gmail.com>
2024-04-25 13:13:14 +0200

    fixed cards and payment

--- src/js/main.js
+++ src/js/main.js
@@ -229,6 +229,14 @@ function InitializeSliders(){
 		customPaging : function(slider, i) {
 			return `<div class="bg-gray-200 cursor-pointer w-full h-full transition duration-300 ease-out-wobble rounded-full "></div>`;
 		},
+		responsive: [
+			{
+				breakpoint: 767,
+				settings: {
+					arrows: false,
+				}
+			},
+		]
 	});
 	$(".banner-slider").slick({
 		slidesToShow: 1,
